943ad3c420a2811fa3a8ab0603eb3639be578e40
Faenzi Lorenzo <faenzilorenzo@gmail.com>
2023-03-04 11:32:39 +0100

    feat: static single post page generation + revalidate

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -15,15 +15,26 @@ type Props = {
   };
 };
 
-const query = groq`
+export const revalidate = 60 * 60 * 24; // revalidate this page every day
+
+export const generateStaticParams = async () => {
+  const query = groq`
+  *[_type=='post'] {
+    slug
+  }
+  `;
+  const posts: Post[] = await client.fetch(query);
+  return posts.map(({slug}) => ({slug: slug.current}));
+};
+
+export default async function Post({params: {slug}}: Props) {
+  const query = groq`
 *[_type=='post' && slug.current == $slug][0] {
   ...,
   author->,
   categories[]->
 }
 `;
-
-export default async function Post({params: {slug}}: Props) {
   const post: Post = await client.fetch(query, {slug});
 
   return (
